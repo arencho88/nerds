f69de57a62dc27883d1ca765423bbf8074eb194d
arencho88 <california0107@mail.ru>
2021-02-18 23:27:26 +0300

    finish catalog page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('.modal')
 const modalOverlay = document.querySelector('.modal-overlay')
 const nameInput = document.querySelector('#name')
+const rangeSlider = document.querySelector('#range_slider')
 
 const modalOpenBtn = document.querySelector('.footer__info-btn')
 const modalCloseBtn = document.querySelector('.modal__close-btn')
@@ -30,3 +31,24 @@ window.addEventListener('keydown', function(e){
 		modalOverlay.classList.remove('modal-overlay__active')	
 	}
 })
+
+if(rangeSlider) {
+	noUiSlider.create(rangeSlider, {
+		start: [0, 15000],
+		connect: true,
+		step: 500,
+		margin: 4000,
+		range: {
+			 'min': 0,
+			 'max': 15000
+		}
+  	});
+
+  const minInput = document.querySelector('#min_inp')
+  const maxInput = document.querySelector('#max_inp')
+  const inputs = [minInput, maxInput]
+
+  	rangeSlider.noUiSlider.on('update', function(values, handle){
+		inputs[handle].value = Math.round(values[handle])	
+  	})
+}
